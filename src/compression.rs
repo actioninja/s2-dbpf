abfbbc91d9801d32aeef654d472ff9aeca1f1abb
actioninja <actioninja@criticalaction.net>
2021-10-21 06:03:53 -0700

    still to be debugged compression

--- src/compression.rs
+++ src/compression.rs
@@ -22,71 +22,91 @@ struct CompressedFile {
 }
 
 impl CompressedFile {
+    fn compress(in_vec: &Vec<u8>) -> CompressedFile {
+        todo!("Todo!")
+    }
+
     fn decompress(&self) -> Vec<u8> {
         let compressed = &self.data;
         let mut decompressed: Vec<u8> = vec![0x00; self.header.uncompressed_size as usize];
 
-        let mut index = 0;
+        let mut index: usize = 0;
         let mut decomp_index: usize = 0;
-        let mut control: u8 = 0;
         println!("compressed length: {}", compressed.len());
         while index < compressed.len() {
-            control = compressed[index];
-            println!("Control character: 0x{:X?} at pos {:?}", control, index);
-            index += 1;
+            let control = &compressed[index];
+            println!("Control character: {:#X}", control);
 
             let control_result: (Option<usize>, Option<(usize, usize)>) = match control {
                 0x00..=0x7F => {
-                    let control_2 = compressed[index];
-                    index += 1;
-                    let num_plain_text: usize = (control_2 & 0b0000_0011) as usize;
+                    let control_slice = &compressed[index..index + 2];
+                    index += 2;
+                    let num_plain_text: usize = (control_slice[0] & 0b0000_0011) as usize;
 
-                    let num_to_copy
+                    let offset: usize = (((((control_slice[0] & 0b0110_0000) as u16) << 3) | (control_slice[1] as u16)) + 1) as usize;
+                    let number_copy_offset = (((control_slice[0] & 0b0001_1100) >> 2) + 3) as usize;
 
-                    let offset: usize = ((((control & 0x60) as u32) << 3) + (control_2 as u32) + 1) as usize;
-                    let number_copy_offset = (((control & 0x1C) >> 2) + 3) as usize;
-                    let start_index: usize = decomp_index - offset;
-                    decompressed[decomp_index..decomp_index + number_copy_offset].copy_from_slice(&compressed[start_index..start_index + number_copy_offset]);
-
-                    (Some(num_plain_text), Some((0, 0)))
+                    (Some(num_plain_text), Some((offset, number_copy_offset)))
                 }
                 0x80..=0xBF => {
-                    (None, None)
+                    let control_slice = &compressed[index..index + 3];
+                    index += 3;
+
+                    let num_plain_text: usize = ((control_slice[1] & 0b1100_0000) >> 6) as usize;
+
+                    let offset: usize = ((((control_slice[1] & 0b0011_1111) as u16) << 8) | (control_slice[2] as u16)) as usize;
+                    let num_to_copy: usize = (control_slice[0] & 0b0011_1111) as usize;
+
+                    (Some(num_plain_text), Some((offset, num_to_copy)))
                 }
                 0xC0..=0xDF => {
-                    (None, None)
+                    let control_slice = &compressed[index..index + 4];
+                    index += 4;
+
+                    let num_plain_text: usize = (control_slice[0] & 0b0000_0011) as usize;
+
+                    let offset: usize = ((((control_slice[0] & 0b0001_0000) as u32) << 12) | ((control_slice[1] as u32) << 8) | (control_slice[2] as u32) + 1) as usize;
+                    let num_to_copy: usize = ((((control_slice[0] & 0b0000_1100) as u16) << 6) | (control_slice[3] as u16) + 5) as usize;
+
+                    (Some(num_plain_text), Some((offset, num_to_copy)))
                 }
                 0xE0..=0xFB => {
+                    let control = compressed[index];
+                    index += 1;
                     //It just is bit shifted, don't ask me why
-                    let number_plain_text = ((control & 0b0001_1111) << 2) as usize;
+                    let number_plain_text = (((control & 0b0001_1111) << 2) + 4) as usize;
                     (Some(number_plain_text), None)
                 }
                 0xFC..=0xFF => {
+                    let control = compressed[index];
+                    index += 1;
                     let number_plain_text = (control & 0b0000_0011) as usize;
                     (Some(number_plain_text), None)
                 }
-                _ => panic!("Invalid control code found!")
             };
 
             if let Some(plaintext_copy) = control_result.0 {
-                println!("Copying: {}", plaintext_copy);
+                println!("Copying plaintext of length: {}", plaintext_copy);
                 let src_slice = &compressed[index..index + plaintext_copy];
                 decompressed[decomp_index..decomp_index + plaintext_copy].copy_from_slice(src_slice);
-                index += number_plain_text;
-                decomp_index += number_plain_text;
+                index += plaintext_copy;
+                decomp_index += plaintext_copy;
             }
 
             if let Some((src_pos, length)) = control_result.1 {
+                println!("Copying length {} to {}", length, src_pos);
+                println!("decomp index: {}", decomp_index);
                 let dest_pos = decomp_index - src_pos;
+                println!("dest pos: {}", dest_pos);
 
-                let src_slice = &decompressed[src_pos..src_pos + length];
-                decompressed[dest_pos..dest_pos + length].copy_from_slice(src_slice);
+                let (dst, src) = decompressed.split_at_mut(src_pos);
+
+                dst[dest_pos..dest_pos + length].copy_from_slice(&src[..length]);
 
                 decomp_index += length;
 
             }
         }
-
 
         decompressed
     }
@@ -103,7 +123,7 @@ fn vec_to_compressed(in_vec: &Vec<u8>) -> Option<CompressedFile> {
 }
 
 mod tests {
-    use crate::compression::vec_to_compressed;
+    use crate::compression::{CompressedFile, vec_to_compressed};
 
     #[test]
     fn is_compressed_works() {
@@ -111,5 +131,16 @@ mod tests {
         let compressed = vec_to_compressed(&data).unwrap();
         println!("{:X?}", compressed);
         let decompressed = compressed.decompress();
+    }
+
+    #[test]
+    fn compression_then_decompression_gives_same_vec() {
+        let test_vec = b"This is a test vector with a repeated phrase! This is a test vector with a repeated phrase!".to_vec();
+
+        let compressed = CompressedFile::compress(&test_vec);
+        let decompressed = compressed.decompress();
+
+        assert_eq!(test_vec, decompressed)
+
     }
 }
